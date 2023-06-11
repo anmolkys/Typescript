8488fa2f3dd7d696128026f25475029f41e9b26c
Anmol <iamanmolbhardwaj@gmail.com>
2023-06-11 10:53:57 +0530

    add: first lesson

---
+++ Lesson 1/sandbox.js
@@ -0,0 +1,7 @@
+var character = "luigi";
+console.log(character);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (input) {
+    console.log(input);
+});
---
+++ Lesson 1/sandbox.ts
@@ -0,0 +1,12 @@
+const character = "luigi";
+
+console.log(character);
+
+const input = document.querySelectorAll('input');
+
+console.log(input);
+
+
+ input.forEach(input =>{
+    console.log(input);
+ })
